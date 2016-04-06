2778653904a3f2ac259f2e89a36110fe8588c371
Joel Ray Holveck <joelh@piquan.org>
2016-04-05 18:35:52 -0700

    Initial Commit

---
+++ Fifty-Five and Five/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fifty-Five and Five
+//
+//  Created by Joel Ray Holveck on 4/5/16.
+//  Copyright © 2016 Joel Ray Holveck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Fifty-Five and Five/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Fifty-Five and Five
+//
+//  Created by Joel Ray Holveck on 4/5/16.
+//  Copyright © 2016 Joel Ray Holveck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
