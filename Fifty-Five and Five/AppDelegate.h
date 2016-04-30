6240c26e754b8c27f76a6c8090b4f532158790e0
Joel Ray Holveck <joelh@piquan.org>
2016-04-29 19:10:35 -0700

    Add copyright / GPL / CC comments.

--- Fifty-Five and Five/AppDelegate.h
+++ Fifty-Five and Five/AppDelegate.h
@@ -1,9 +1,20 @@
 //
 //  AppDelegate.h
 //  Fifty-Five and Five
+//  Copyright © 2016 Joel Ray Holveck
 //
-//  Created by Joel Ray Holveck on 4/5/16.
-//  Copyright © 2016 Joel Ray Holveck. All rights reserved.
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 #import <UIKit/UIKit.h>
